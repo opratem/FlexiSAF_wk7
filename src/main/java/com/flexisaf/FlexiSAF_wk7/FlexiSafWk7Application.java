d2d81a1b0e94001e9bf12bf49919133bcbd17256
opratem <praiseolufemi1@gmail.com>
2025-10-23 11:57:37 +0100

    First commit

---
+++ src/main/java/com/flexisaf/FlexiSAF_wk7/FlexiSafWk7Application.java
@@ -0,0 +1,13 @@
+package com.flexisaf.FlexiSAF_wk7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexiSafWk7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexiSafWk7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/flexisaf/FlexiSAF_wk7/FlexiSafWk7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flexisaf.FlexiSAF_wk7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexiSafWk7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
